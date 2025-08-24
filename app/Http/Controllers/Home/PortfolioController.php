4f0869cfbb9a54f537a178e722c0e5bad5e12e5b
Md. Gofran Khan <gofran0123@gmail.com>
2025-08-24 16:37:43 +0600

    .env-example added

--- app/Http/Controllers/Home/PortfolioController.php
+++ app/Http/Controllers/Home/PortfolioController.php
@@ -9,101 +9,105 @@ use Image;
 
 class PortfolioController extends Controller
 {
-    public function AllPortfolio(){
+    public function AllPortfolio()
+    {
 
         $portfolio = Portfolio::latest()->get();
         return view('admin.portfolio.portfolio_all', compact('portfolio'));
     } //End of method
 
-    public function AddPortfolio(){
+    public function AddPortfolio()
+    {
 
         return view('admin.portfolio.portfolio_add');
     } //End of method
 
-    public function StorePortfolio(Request $request){
+    public function StorePortfolio(Request $request)
+    {
 
         $request->validate([
             'portfolio_name' => 'required',
             'portfolio_title' => 'required',
             'portfolio_image' => 'required',
-        ],[
+        ], [
             'portfolio_name.required' => 'Portfolio Name is Required',
             'portfolio_title.required' => 'Portfolio Titile is Required',
         ]);
         $image = $request->file('portfolio_image');
-            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();  // 3434343443.jpg
-            Image::make($image)->resize(1020,519)->save('upload/portfolio/'.$name_gen);
-            $save_url = 'upload/portfolio/'.$name_gen;
-            Portfolio::insert([
-                'portfolio_name' => $request->portfolio_name,
-                'portfolio_title' => $request->portfolio_title,
-                'portfolio_description' => $request->portfolio_description,
-                'portfolio_image' => $save_url,
-                'created_at' => Carbon::now(),
-            ]); 
-            $notification = array(
-            'message' => 'Portfolio Inserted Successfully', 
+        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();  // 3434343443.jpg
+        Image::make($image)->resize(1020, 519)->save('upload/portfolio/' . $name_gen);
+        $save_url = 'upload/portfolio/' . $name_gen;
+        Portfolio::insert([
+            'portfolio_name' => $request->portfolio_name,
+            'portfolio_title' => $request->portfolio_title,
+            'portfolio_description' => $request->portfolio_description,
+            'portfolio_image' => $save_url,
+            'created_at' => Carbon::now(),
+        ]);
+        $notification = array(
+            'message' => 'Portfolio Inserted Successfully',
             'alert-type' => 'success'
         );
         return redirect()->route('all.portfolio')->with($notification);
-
     } //End of method
 
-    public function EditPortfolio($id){
+    public function EditPortfolio($id)
+    {
         $portfolio = Portfolio::findOrFail($id);
         return view('admin.portfolio.portfolio_edit', compact('portfolio'));
-
     } //End of method
 
-    public function UpdatePortfolio(Request $request){
+    public function UpdatePortfolio(Request $request)
+    {
         $portfolio_id = $request->id;
         if ($request->file('portfolio_image')) {
             $image = $request->file('portfolio_image');
-            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();  // 3434343443.jpg
-            Image::make($image)->resize(1020,519)->save('upload/portfolio/'.$name_gen);
-            $save_url = 'upload/portfolio/'.$name_gen;
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();  // 3434343443.jpg
+            Image::make($image)->resize(1020, 519)->save('upload/portfolio/' . $name_gen);
+            $save_url = 'upload/portfolio/' . $name_gen;
             Portfolio::findOrFail($portfolio_id)->update([
                 'portfolio_name' => $request->portfolio_name,
                 'portfolio_title' => $request->portfolio_title,
                 'portfolio_description' => $request->portfolio_description,
                 'portfolio_image' => $save_url,
-            ]); 
+            ]);
             $notification = array(
-            'message' => 'Portfolio Updated with Image Successfully', 
-            'alert-type' => 'success'
-        );
-        return redirect()->route('all.portfolio')->with($notification);
-        } else{
+                'message' => 'Portfolio Updated with Image Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->route('all.portfolio')->with($notification);
+        } else {
             Portfolio::findOrFail($portfolio_id)->update([
                 'portfolio_name' => $request->portfolio_name,
                 'portfolio_title' => $request->portfolio_title,
                 'portfolio_description' => $request->portfolio_description,
-                 
-            ]); 
-            $notification = array(
-            'message' => 'Portfolio Updated without Image Successfully', 
-            'alert-type' => 'success'
-        );
-       return redirect()->route('all.portfolio')->with($notification);
-        } // end Else
-     } // End Method 
 
-     public function DeletePortfolio($id){
+            ]);
+            $notification = array(
+                'message' => 'Portfolio Updated without Image Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->route('all.portfolio')->with($notification);
+        } // end Else
+    } // End Method 
+
+    public function DeletePortfolio($id)
+    {
         $portfolio = Portfolio::findOrFail($id);
         $img = $portfolio->portfolio_image;
         unlink($img);
         Portfolio::findOrFail($id)->delete();
-         $notification = array(
-            'message' => 'Portfolio Image Deleted Successfully', 
+        $notification = array(
+            'message' => 'Portfolio Image Deleted Successfully',
             'alert-type' => 'success'
         );
-        return redirect()->back()->with($notification);       
-     }// End Method 
+        return redirect()->back()->with($notification);
+    } // End Method 
 
-     public function PortfolioDetails($id){
+    public function PortfolioDetails($id)
+    {
 
         $portfolio = Portfolio::findOrFail($id);
         return view('frontend.portfolio_details', compact('portfolio'));
-
-     }
+    }
 }
