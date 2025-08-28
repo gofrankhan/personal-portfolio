69472765f0223241d32a7d4f9161c376d70cd9c2
Md. Gofran Khan <gofran0123@gmail.com>
2025-08-28 11:14:29 +0600

    validate error message added to add blog category

--- app/Http/Controllers/Home/PortfolioController.php
+++ app/Http/Controllers/Home/PortfolioController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Portfolio;
+use Illuminate\Support\Carbon;
 use Image;
 
 class PortfolioController extends Controller
--- resources/views/admin/blog_category/blog_category_add.blade.php
+++ resources/views/admin/blog_category/blog_category_add.blade.php
@@ -1,56 +1,81 @@
 @extends('admin.admin_master')
 @section('admin')
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<div class="page-content">
-    <div class="container-fluid">
+    <div class="page-content">
+        <div class="container-fluid">
 
-        <!-- start page title -->
-        <div class="row">
-            <div class="col-12">
-                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0">Add Blog Category</h4>
+            <!-- start page title -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                        <h4 class="mb-sm-0">Add Blog Category</h4>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- end page title -->
-        
-        <!-- start page title -->
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                    <form method="post" action="{{ route('store.blog.category') }}" >
-                        @csrf
-                        <div class="row mb-3">
-                            <label for="blog_category" class="col-sm-2 col-form-label">Blog Category Name</label>
-                            <div class="col-sm-10">
-                                <input class="form-control" name="blog_category" type="text" id="blog_category" value="">
-                                @error('blog_category')
-                                    <span class="text-danger"> {{ $message }} </span>
-                                @enderror
-                            </div>
+            <!-- end page title -->
+
+            <!-- start page title -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <form method="post" id="myForm" action="{{ route('store.blog.category') }}">
+                                @csrf
+                                <div class="row mb-3">
+                                    <label for="blog_category" class="col-sm-2 col-form-label">Blog Category Name</label>
+                                    <div class="form-group col-sm-10">
+                                        <input class=" form-control" name="blog_category" type="text" id="blog_category"
+                                            value="">
+                                    </div>
+                                </div>
+
+                                <input type="submit" class="btn btn-primary btn-rounded waves-effect waves-light"
+                                    value="Insert Blog Category">
+                            </form>
                         </div>
-
-                        <input type="submit"  class="btn btn-primary btn-rounded waves-effect waves-light" value="Insert Blog Category">
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 
-<script type="text/javascript">
-    
-    $(document).ready(function(){
-        $('#image').change(function(e){
-            var reader = new FileReader();
-            reader.onload = function(e){
-                $('#showImage').attr('src',e.target.result);
-            }
-            reader.readAsDataURL(e.target.files['0']);
-        });
-    });
-</script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $('#image').change(function(e) {
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                        $('#showImage').attr('src', e.target.result);
+                    }
+                    reader.readAsDataURL(e.target.files['0']);
+                });
+            });
+        </script>
 
-@endsection
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $('#myForm').validate({
+                    rules: {
+                        blog_category: {
+                            required: true,
+                        },
+                    },
+                    messages: {
+                        blog_category: {
+                            required: 'Please Enter Blog Category',
+                        },
+                    },
+                    errorElement: 'span',
+                    errorPlacement: function(error, element) {
+                        error.addClass('invalid-feedback');
+                        element.closest('.form-group').append(error);
+                    },
+                    highlight: function(element, errorClass, validClass) {
+                        $(element).addClass('is-invalid');
+                    },
+                    unhighlight: function(element, errorClass, validClass) {
+                        $(element).removeClass('is-invalid');
+                    },
+                });
+            });
+        </script>
+    @endsection
